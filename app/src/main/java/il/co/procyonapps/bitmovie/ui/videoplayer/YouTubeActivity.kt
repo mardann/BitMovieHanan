af8fe6deee21eb5198326f535045882bb7883149
hanandann <hanan.dann@tipranks.com>
2022-11-17 13:25:37 +0200

    video player optimized

--- app/src/main/java/il/co/procyonapps/bitmovie/ui/videoplayer/YouTubeActivity.kt
+++ app/src/main/java/il/co/procyonapps/bitmovie/ui/videoplayer/YouTubeActivity.kt
@@ -1,45 +1,59 @@
 package il.co.procyonapps.bitmovie.ui.videoplayer
 
-
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.View
 import androidx.navigation.navArgs
 import com.google.android.youtube.player.*
 import il.co.procyonapps.bitmovie.api.APIConst
 import il.co.procyonapps.bitmovie.databinding.FragmntYoutubeBinding
 
-class YouTubeActivity: YouTubeBaseActivity() {
+class YouTubeActivity : YouTubeBaseActivity() {
     val TAG = this::class.simpleName ?: "Unspecified"
-    
-    val args : YouTubeActivityArgs by navArgs()
-    
+    val args: YouTubeActivityArgs by navArgs()
     var binder: FragmntYoutubeBinding? = null
     override fun onCreate(p0: Bundle?) {
         super.onCreate(p0)
         binder = FragmntYoutubeBinding.inflate(layoutInflater)
         setContentView(binder!!.root)
+        setActionBar(binder!!.myToolbar)
+        
+        actionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeButtonEnabled(true)
+        }
+        
         
         onViewCreated(binder!!.root, p0)
-        
     }
-   
     
     private fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        
         binder!!.player.initialize(APIConst.YOUTUBE_DEV_KEY, playerInitialization)
     }
     
-    private val playerInitialization = object: YouTubePlayer.OnInitializedListener{
-        override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
-            if(!wasRestored && player != null){
-                player.cueVideo(args.videoId)
-                player.play()
-                
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                this.finish()
+                return true
             }
         }
+        return super.onOptionsItemSelected(item)
+    }
+    
+    private val playerInitialization = object : YouTubePlayer.OnInitializedListener {
+        override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, player: YouTubePlayer?, wasRestored: Boolean) {
+            if (!wasRestored && player != null) {
+                player.cueVideo(args.videoId)
+                binder?.player?.postDelayed({
+                    player.play()
+                }, 2000)
+            }
+        }
+        
         override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
-            Log.e(TAG, "onInitializationFailure: ${p1?.name}", )
+            Log.e(TAG, "onInitializationFailure: ${p1?.name}")
         }
     }
     
@@ -47,6 +61,4 @@ class YouTubeActivity: YouTubeBaseActivity() {
         super.onDestroy()
         binder = null
     }
-
-
 }
